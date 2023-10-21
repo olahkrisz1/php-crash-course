3e5fb6a4dac6a6a12315455bdde03cca9bff4837
olahkrisz1 <olahkrisz1@gmail.com>
2023-10-21 18:44:32 +0200

    oop methods

---
+++ 17_oop.php
@@ -0,0 +1,72 @@
+<?php
+/* --- Object Oriented Programming -- */
+
+/*
+  From PHP5 onwards you can write PHP in either a procedural or object oriented way. OOP consists of classes that can hold "properties" and "methods". Objects can be created from classes.
+*/
+
+class User
+{
+    // properties are attributes that belong to a class
+
+    // Access Modifiers: public, private, protected
+    // public - can be accessed from anywhere
+    // private - can be accessed only from inside the class
+    // proected - can only be accessed from inside the class and by inheriting classes
+    public $name;
+    public $email;
+    public $password;
+
+    // a constructor is a method that runs when an object is created
+    public function __construct($name, $email, $password)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    // Method is a function that belongs to a class
+    function set_name($name)
+    {
+        $this->name = $name;
+    }
+
+    function get_name()
+    {
+        return $this->name;
+    }
+}
+
+// Instatiate a user object
+$user1 = new User('brad', 'brad@gmail.com', '34333');
+$user2 = new User('John', 'john@gmail.com', 'ddog12');
+
+echo $user1->email;
+echo $user2->name;
+
+// $user1->set_name('John');
+// $user2->set_name('Paul');
+
+// echo $user1->get_name();
+// echo $user2->get_name();
+
+
+// Inheritance
+
+class Employee extends User
+{
+    public function __construct($name, $email, $password, $title)
+    {
+        parent::__construct($name, $email, $password);
+        $this->title = $title;
+    }
+
+    public function get_title()
+    {
+        return $this->title;
+    }
+}
+
+$employee1 = new Employee('Sara', 'sara@gmail.com', '4343', 'manager');
+
+echo $employee1->get_title();
